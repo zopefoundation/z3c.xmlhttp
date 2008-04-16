74bd47b7def6278cff046be4acae43ae7f858686
Roger Ineichen <roger@projekt01.ch>
2008-04-16 01:12:40 +0000

    vb

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.xmlhttp',
-    version='0.5.0',
+    version='0.5.1dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "XMLHttpRequest (XHR) javascript",
